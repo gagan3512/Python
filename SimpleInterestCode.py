8edd092f9ca16aa3e6260b29ffbd639f43a213f0
Gagan Tuli <gagan1825.cse18@chitkara.edu.in>
2021-08-23 13:12:22 +0530

    Create SimpleInterestCode.py

---
+++ SimpleInterestCode.py
@@ -0,0 +1,10 @@
+p = input('Enter the value of principal')
+n = input('Enter the number of years')
+r = input('Enter the rate of interest')
+
+p = int(p)
+n = int(n)
+r = int(r)
+
+simpleInterest = (p*n*r)/100
+print(simpleInterest)
